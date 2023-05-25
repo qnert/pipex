2e4da1aa8a6472ab6e9d85faf2549a3d738da6ef
Simon Kunert <skunert@1-A-4.42heilbronn.de>
2023-05-25 12:50:50 +0200

    norminette change

--- pipex_utils/pipex_bonus.c
+++ pipex_utils/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 09:04:30 by skunert           #+#    #+#             */
-/*   Updated: 2023/05/25 11:39:30 by skunert          ###   ########.fr       */
+/*   Updated: 2023/05/25 12:48:32 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,23 @@ int	main(int argc, char **argv, char **envp)
 	int	fd_in;
 	int	fd_out;
 	int	fd[2];
-	int	pid;
 	int	i;
 
 	if (argc >= 6)
 	{
-		pid = 0;
 		fd_in = ft_infile_check(argv[1]);
 		fd_out = ft_outfile_check(argv[argc - 1]);
 		if (fd_in != -1)
-			dup2(fd_in, STDIN_FILENO);
-		i = 2;
-		while (i < argc - 2 && fd_in != -1)
 		{
-			pid = fork_child_proc(argv, envp, i, fd);
-			if (pid == -1)
-				return (-1);
-			i++;
+			dup2(fd_in, STDIN_FILENO);
+			if (pipe(fd) == -1)
+				return (perror("Error"), -1);
 		}
-		if (pipe(fd) == -1)
-			return (perror("Error"), -1);
+		i = 1;
+		while (++i < argc - 2 && fd_in != -1)
+		{
+			fork_child_proc(argv, envp, i, fd);
+		}
 		fork_2(argv + (argc - 5), envp, fd, fd_out);
 	}
 	return (0);
